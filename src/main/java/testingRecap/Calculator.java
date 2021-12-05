ab94f7b9a70a74ae82ac0a21679c9c6434f5fdee
AgZid <zidoniene.agne@gmail.com>
2021-12-05 13:18:11 +0200

    Added tests and methods

--- src/main/java/testingRecap/Calculator.java
+++ src/main/java/testingRecap/Calculator.java
@@ -2,6 +2,8 @@ package testingRecap;
 
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
+
 public class Calculator {
 
 
@@ -17,11 +19,29 @@ public class Calculator {
     }
 
     public int addMultipleNumbers(int[] numbers) {
+        LOGGER.info("Adding multiple numbers" + Arrays.toString(numbers));
         int sum = 0;
         for (int number : numbers) {
             sum += number;
         }
 
         return sum;
+
+    }
+
+
+    public int subtractTwoNumbers(int firstNumber, int secondNumber) {
+        LOGGER.info("Substract two numbers " + firstNumber + " and " + secondNumber);
+        return firstNumber - secondNumber;
+    }
+
+    public int subtractMultipleNumbers(int[] numbers) {
+        LOGGER.info("Substract multiple numbers" + Arrays.toString(numbers));
+        int subtract = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            subtract -= numbers[i];
+        }
+
+        return subtract;
     }
 }
--- src/test/java/testingRecap/CalculatorTest.java
+++ src/test/java/testingRecap/CalculatorTest.java
@@ -2,13 +2,54 @@ package testingRecap;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 class CalculatorTest {
+    private Calculator calculator = new Calculator();
 
     @Test
-    void addTwoNumbers() {
+    void testAddTwoNumbers() {
+        int firstNumber = 1;
+        int secondNumber = 10;
+
+        int actualAddResult = calculator.addTwoNumbers(firstNumber, secondNumber);
+
+        assertEquals(11, actualAddResult);
+        assertThat(actualAddResult).isEqualTo(11); //analogiskas tikrinimas su assertj
     }
 
     @Test
-    void addMultipleNumbers() {
+    void testAddMultipleNumbers() {
+        int[] numbersToTest = {5 ,2, -3, 4, 1};
+
+        int actualResult = calculator.addMultipleNumbers(numbersToTest);
+
+        assertThat(actualResult).isEqualTo(9);
+        //assertEquals(15,actualResult);
     }
+
+    @Test
+    void testSubtractTwoNumbers() {
+        int firstNumber = 1;
+        int secondNumber = 10;
+
+        int actualAddResult = calculator.subtractTwoNumbers(firstNumber, secondNumber);
+
+        assertEquals(-9, actualAddResult);
+        assertThat(actualAddResult).isEqualTo(-9); //analogiskas tikrinimas su assertj
+    }
+
+    @Test
+    void testSubtractMultipleNumbers() {
+        int[] numbersToTest = {5 ,2, -3, 4, 1};
+
+        int actualResult = calculator.subtractMultipleNumbers(numbersToTest);
+
+        assertThat(actualResult).isEqualTo(1);
+        //assertEquals(15,actualResult);
+    }
+
+
 }
